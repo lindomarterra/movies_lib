a7267c93041569f1bf1f7f8e1de6db3ed1fd901a
Terra <terra.lindomar@gmail.com>
2024-06-18 20:04:45 -0300

    inicial commit

---
+++ src/components/narbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
+
+const Narbar = () => {
+  return (
+    <div className="navbar">
+      <h2>
+        <Link to="/">
+          <BiCameraMovie /> MoviesLib{' '}
+        </Link>
+      </h2>
+      <form>
+        <input type="text" placeholder="Busque um filme" />
+        <button type="submit">          
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Narbar
---
+++ src/pages/movie.jsx
@@ -0,0 +1,9 @@
+const Movie = () => {
+  return (
+    <div>
+      <h1> MOVIE </h1>
+    </div>
+  )
+}
+
+export default Movie
